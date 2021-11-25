1e58490f7ea5e0b9750b6fee0be0eaeee2f91296
Jan Hinterholzinger <hintik@icloud.com>
2021-11-25 22:41:33 +0100

    Aktualizace souboru

--- linkedlist.h
+++ linkedlist.h
@@ -3,6 +3,9 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+
+#include <stdio.h>
 
 typedef struct _linked_list {
     struct linked_list_item *first;
@@ -23,8 +26,8 @@ int linked_list_add(linked_list *ls, char *name);
 
 int linked_list_remove_last(linked_list *ls);
 
-int linked_list_free(linked_list *ls);
+int linked_list_add_to_start(linked_list *ls, char *name);
 
-
+int linked_list_free(linked_list **ls);
 
 #endif
